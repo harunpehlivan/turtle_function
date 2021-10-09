0c20f9ebbd5fe1930c53146a39707e36002e45a4
HARUN PEHLİVAN <trcmnhp@hotmail.com>
2021-10-09 13:56:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+def myTurtle(forward, rotateLeft):
+    my_turtle.forward(forward)
+    my_turtle.left(rotateLeft)
+    my_turtle.forward(forward)
+    my_turtle.left(rotateLeft)
+    my_turtle.forward(forward)
+    my_turtle.left(rotateLeft)
+    my_turtle.forward(forward)
+
+myTurtle(90, 90)
+my_turtle.forward(90)
+myTurtle(90, 90)
+myTurtle(90, 90)
+myTurtle(90, 90)
+myTurtle(90, 90)
+my_turtle.forward(90)
+my_turtle.left(90)
+my_turtle.forward(90)
+myTurtle(90, 90)
+my_turtle.right(180)
+my_turtle.forward(90)
+my_turtle.right(90)
+myTurtle(90, 90)
+my_turtle.right(180)
+my_turtle.forward(90)
+my_turtle.right(90)
+myTurtle(90, 90)
